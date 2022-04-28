52be10616c782919db0265802128e179175f389b
Steve Han <stevehan2001@gmail.com>
2022-04-28 18:54:35 -0500

    moving to new repo

---
+++ baseline.py
@@ -0,0 +1,19 @@
+from sim import Sim
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+env = Sim(60)
+
+env.reset()
+
+#model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./test_tensorboard2")
+#model.learn(total_timesteps=90000)
+
+#model.save("saved")
+model = PPO.load("saved", env=env)
+
+#mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+
+#print(f"mean_reward:{mean_reward:.2f} +/- {std_reward: .2f}")
+
+env.render_episode(model, 5)
